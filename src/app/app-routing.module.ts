1e078e7e4390348786dd45d4fcfe36237d51b4c2
AngelRivera <20233l001142@utcv.edu.mx>
2025-03-18 21:24:28 -0600

    feat: enrutar paginas de login y register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth', //Ruta padre para autenticacion
+    loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule),
+  },
+  
+  {
+    path: '', //Ruta padre(inicial) para cualquier otra ruta
+    redirectTo: '/auth/login', //Redirecciona a la ruta login
+    pathMatch: 'full', //Solo se redirecciona si la ruta es exactamente vacia
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PagesRoutingModule } from './pages-routing.module';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ComponentsModule } from '../components/components.module';
+
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    CommonModule,
+    PagesRoutingModule,
+    ComponentsModule
+  ]
+})
+export class PagesModule { }
