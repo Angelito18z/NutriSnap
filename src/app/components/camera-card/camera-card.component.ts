bf8897f01a8f55f2565b98680f298686da4382ad
Christba <christba455@gmail.com>
2025-03-19 12:18:22 -0600

    feat:componets setup

--- src/app/components/camera-card/camera-card.component.ts
+++ src/app/components/camera-card/camera-card.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-camera-card',
-  standalone: false,
   templateUrl: './camera-card.component.html',
-  styleUrl: './camera-card.component.scss'
+  styleUrls: ['./camera-card.component.scss']
 })
 export class CameraCardComponent {
+  @ViewChild('video') videoElement!: ElementRef;
+  isCameraActive = false;
 
+  async startCamera() {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      this.videoElement.nativeElement.srcObject = stream;
+      this.isCameraActive = true;
+    } catch (error) {
+      console.error('Error accessing webcam:', error);
+    }
+  }
 }
