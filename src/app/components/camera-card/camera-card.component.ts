510341e31e70f09a63995f3aa393c456b96b0189
Christba03 <christba455@gmail.com>
2025-03-19 19:23:33 -0600

    feat:angular material installed

---
+++ src/app/components/camera-card/camera-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-camera-card',
+  standalone: false,
+  templateUrl: './camera-card.component.html',
+  styleUrl: './camera-card.component.scss'
+})
+export class CameraCardComponent {
+
+}
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
 import { FormComponent } from './form/form.component';
 import { FooterComponent } from './footer/footer.component';
 import { FoodCardComponent } from './food-card/food-card.component';
@@ -7,6 +9,7 @@ import { NutrientBarComponent } from './nutrient-bar/nutrient-bar.component';
 import { TableContentsComponent } from './table-contents/table-contents.component';
 import { ReviewCardComponent } from './review-card/review-card.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CameraCardComponent } from './camera-card/camera-card.component';
 
 @NgModule({
   declarations: [
@@ -16,13 +19,23 @@ import { NavbarComponent } from './navbar/navbar.component';
     FoodCardComponent,
     NutrientBarComponent,
     TableContentsComponent,
-    ReviewCardComponent
+    ReviewCardComponent,
+    CameraCardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatCardModule
   ],
-  exports:[
-    FormComponent
+  exports: [
+    FormComponent,
+    NavbarComponent,
+    FooterComponent,
+    FoodCardComponent,
+    NutrientBarComponent,
+    TableContentsComponent,
+    ReviewCardComponent,
+    CameraCardComponent
   ]
 })
 export class ComponentsModule { }
