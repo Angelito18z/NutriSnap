f6a6ae87d5e1b2fcf16b157ba513eeb2bd462b60
AngelRivera <20233l001142@utcv.edu.mx>
2025-03-19 19:53:36 -0600

    feat: agregar paginas de login y register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,16 +6,16 @@ const routes: Routes = [
     path: 'auth', //Ruta padre para autenticacion
     loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule),
   },
-  
+  {
+    path: 'landing',
+    loadChildren: () => import('./pages/landing/landing.module').then(m => m.LandingModule),
+  },
   {
     path: '', //Ruta padre(inicial) para cualquier otra ruta
     redirectTo: '/auth/login', //Redirecciona a la ruta login
     pathMatch: 'full', //Solo se redirecciona si la ruta es exactamente vacia
   },
-  {
-    path: 'landing',
-    loadChildren: () => import('./pages/landing/landing.module').then(m => m.LandingModule),
-  },
+  
 
 ];
 
