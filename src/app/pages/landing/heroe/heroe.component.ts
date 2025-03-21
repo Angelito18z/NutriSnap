35009eaf4218e7fef03a1903a707b7d2f4fc29d0
Christba03 <christba455@gmail.com>
2025-03-20 21:00:08 -0600

    fix: hero section test file
    there was a wrong typo

--- src/app/pages/landing/heroe/heroe.component.ts
+++ src/app/pages/landing/heroe/heroe.component.ts
@@ -1,19 +1,11 @@
 import { Component } from '@angular/core';
-import { Trabajador } from '../../../nutri-data/interfaces/nutri-data';
+
 @Component({
   selector: 'app-heroe',
   standalone: false,
   templateUrl: './heroe.component.html',
   styleUrl: './heroe.component.scss'
 })
-export class HeroeComponents {
-  trabajador: Trabajador = {
-    id: 1,
-    alt_img: 'assets/nutriologos/Angel.jpg',
-    usuario_id: 1,
-    especialidad: '',
-    horario: '',
-    pacientes_asignados: []
-  };
-}
+export class HeroeComponent {
 
+}
